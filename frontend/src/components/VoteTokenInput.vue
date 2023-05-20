a028d76c425c2688200ef572afc2aa361181cd5b
Felix Dangers <f.dangers@ostfalia.de>
2023-05-20 21:17:39 +0200

    Votes can now be displayed with an edit token. Update option is still missing.

--- frontend/src/components/VoteTokenInput.vue
+++ frontend/src/components/VoteTokenInput.vue
@@ -1,55 +1,77 @@
 <template>
     <div>
-        <div class="create-poll">
-            <br><br><br>
-            <form @submit.prevent="submitToken">
-                <label :for="tokenInputId">Insert Edit-Token here:</label>
-                <input :id="tokenInputId" type="text" v-model="token" :disabled="loading" required placeholder="Edit-Token">
-                <button type="submit" :disabled="loading" @click="loadDataFromDatabase">Search</button>
-                <br><br><br>
-                <span v-if="loading">Loading...</span>
-            </form>
-            <form>
-                <label for="question">Title:</label>
-                <input type="text" id="poll_title" v-model="title" :disabled="true">
-                <br><br><br>
-                <label for="options">Vote Options:</label>
-                <div v-for="(option, index) in options" :key="index" class="option-container">
-                    <button :id="'option-' + index"
-                        :class="{ 'green': isOptionClicked[index], 'white': !isOptionClicked[index] }"
-                        @click="toggleOption(index)" :disabled="false">
-                        {{ option.text }}
-                    </button>
-                </div>
-                <br><br><br>
-                <button @click.prevent="submit" :class="{ 'disabled-button': !optionCountValid }" :disabled="!optionCountValid">
-                    Change Vote
-                </button>
-            </form>
+      <div class="create-poll">
+        <br /><br /><br />
+        <form @submit.prevent="submitToken">
+          <div class="form-group">
+            <label :for="tokenInputId">Insert Edit-Token here:</label>
+            <div id="search-field" class="input-container">
+              <input
+                :id="tokenInputId"
+                type="text"
+                v-model="token"
+                :disabled="loading"
+                required
+                placeholder="Edit-Token"
+              />
+              <button id="search" type="submit" :disabled="loading" @click="loadDataFromDatabase">Search</button>
+            </div>
+          </div>
+          <br /><br /><br />
+          <div class="form-group">
+            <label for="question">Title:</label>
+            <input type="text" id="question" v-model="title" :disabled="true" />
+          </div>
+          <br /><br /><br />
+          <div class="form-group">
+            <label for="owner">Owner:</label>
+            <input type="text" id="owner" v-model="vote_owner" :disabled="false" required placeholder="Enter your name here" />
+          </div>
+          <br /><br /><br />
+          <label for="options">Vote Options:</label>
+          <div class="form-group">
+            <div v-for="(option, index) in options" :key="index" class="option-container">
+              <button
+                :id="'option-' + index"
+                :class="{ 'green': isOptionClicked[index], 'white': !isOptionClicked[index] }"
+                @click="toggleOption(index)"
+                :disabled="false"
+              >
+                {{ option.text }}
+              </button>
+            </div>
+          </div>
+          <br /><br /><br />
+          <button id="changevote" @click.prevent="submit" :class="{ 'disabled-button': !optionCountValid }" :disabled="!optionCountValid">
+            Change Vote
+          </button>
+        </form>
+      </div>
+  
+      <div class="options-window">
+        <h2>Poll-Options</h2>
+        <div class="option">
+          <label for="description">Description:</label>
+          <input type="input" id="description" v-model="description" :disabled="true" />
         </div>
-
-        <div class="options-window">
-            <h2>Poll-Options</h2>
-            <div class="option">
-                <label for="description">Description:</label>
-                <input type="input" id="description" v-model="description" :disabled="true">
-            </div>
-            <div class="option">
-                <label for="deadline">Deadline:</label>
-                <input type="input" id="deadline" v-model="deadline" :disabled="true">
-            </div>
-            <div class="option">
-                <label for="max-options">Maximum number of votes:</label>
-                <input type="range" id="max-options" v-model="voices" min="1" :max="options.length" :disabled="true">
-                <span class="range-value">{{ voices }}</span>
-            </div>
-            <div class="option">
-                <label for="worst">Show worst vote too:</label>
-                <input type="checkbox" id="worst" v-model="worst" :disabled="true">
-            </div>
+        <div class="option">
+          <label for="deadline">Deadline:</label>
+          <input type="input" id="deadline" v-model="convertTimestringToReadable" :disabled="true" />
         </div>
+        <div class="option">
+          <label for="max-options">Maximum number of votes:</label>
+          <div class="input-container">
+            <input type="range" id="max-options" v-model="voices" min="1" :max="options.length" :disabled="true" />
+            <span class="range-value">{{ voices }}</span>
+          </div>
+        </div>
+        <div class="option">
+          <label for="worst">Show worst vote too:</label>
+          <input type="checkbox" id="worst" v-model="worst" :disabled="true" />
+        </div>
+      </div>
     </div>
-</template>
+  </template>
 
 <script>
 import axios from 'axios';
@@ -66,6 +88,14 @@ export default {
             }, 0);
             return (count <= this.voices) && (count > 0);
         },
+        convertTimestringToReadable(){
+            const dateString = this.deadline;
+            if(dateString == ''){
+                return '';
+            }
+            const date = new Date(dateString);
+            return date.toLocaleString();
+        },
     },
     data() {
         return {
@@ -74,6 +104,7 @@ export default {
             description: '',
             deadline: '',
             voices: 0,
+            vote_owner: '',
             worst: false,
             token: '',
             selectedOptions: [],
@@ -97,6 +128,7 @@ export default {
                     this.voices = pollBody.setting.voices;
                     this.worst = pollBody.setting.worst;
                     this.selectedOptions = response.data.vote.choice;
+                    this.vote_owner = response.data.vote.owner.name;
 
                     // Setze den Zustand der Buttons basierend auf den ausgewählten Optionen
                     this.isOptionClicked = this.options.map((option) => {
@@ -104,7 +136,6 @@ export default {
                             return selectedOption.id == option.id;
                         })
                     });
-                    console.log(this.isOptionClicked)
                 })
                 .catch(error => {
                     console.error(error);
@@ -122,13 +153,18 @@ export default {
         },
         submit() {
             // Fügen Sie hier den Code ein, um die aktualisierten Daten an die Datenbank zu senden
-
         }
     }
 };
 </script>
 
 <style>
+
+.input-container#search-field {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+
 .create-poll {
     max-width: 600px;
     margin: 0 auto;
@@ -196,7 +232,7 @@ input[type="datetime-local"] {
 
 input[type="number"],
 
-.disabled-button {
+.disabled-button#changevote {
     background-color: #ccc;
     cursor: not-allowed;
 }
@@ -213,12 +249,7 @@ input[type="checkbox"] {
     font-size: 16px;
 }
 
-label {
-    display: block;
-    margin-bottom: 0.5rem;
-    font-size: 18px;
-    font-weight: bold;
-}
+
 
 h2 {
     font-size: 24px;
@@ -227,9 +258,22 @@ h2 {
 
 button {
     display: block;
-    margin-top: 10px;
-    padding: 10px;
+    padding: 0px;
     width: 100%;
+    font-size: 16px;
+    background-color: #008CBA;
+    color: white;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+
+
+button#changevote {
+    display: block;
+    padding: 10px;
+    width: 20%;
     /* Macht alle Buttons gleich breit */
     font-size: 16px;
     background-color: #008CBA;
@@ -237,7 +281,19 @@ button {
     border: none;
     cursor: pointer;
     transition: background-color 0.3s ease;
-    /* Fügt eine flüssige Übergangswirkung hinzu */
+}
+
+button#search {
+    display: block;
+    width: 5em;
+    height: 100%;
+    margin-top: 0px;
+    font-size: 16px;
+    background-color: #008CBA;
+    color: white;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
 }
 
 button:hover {
@@ -247,7 +303,35 @@ button:hover {
 
 .option-container {
     display: flex;
+    flex-basis: 50%;
+    flex-grow: 1;
     align-items: center;
+}
+
+.option-container button {
+    text-align: center;
+    padding: 10px;
+    font-size: 16px;
+    background-color: white;
+    color: black;
+    border: 1px solid #ccc;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    border-radius: 5em;
+}
+
+.option-container button.green {
+    background-color: green;
+    color: white;
+}
+
+.option-container button.white {
+    background-color: white;
+    color: black;
+}
+
+.option-container button:hover:not(.disabled-button) {
+    background-color: #f5f5f5;
 }
 
 .delete-option {
@@ -278,6 +362,26 @@ h2 {
     background-color: white;
     color: black;
 }
+
+.form-group {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+}
+
+.label-container {
+    display: flex;
+    align-items: center;
+    margin-bottom: 0.5rem;
+}
+
+.label-container label {
+    font-size: 18px;
+    font-weight: bold;
+    margin-right: 0.5rem;
+    color: #555;
+}
+
 </style>
 
 
